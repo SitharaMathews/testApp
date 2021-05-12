034016e46add7b9d2822219d94385c94b838c2b3
SitharaMathews <sitharamathews@gmail.com>
2021-05-12 01:34:24 -0400

    add table,fetch response,display sorted data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,68 @@ import { ApiService } from '../app/services/api-service.service';
 
 export class AppComponent implements OnInit {
   title = 'fhir-app-test';
+  patientData:any;
+  patientEntry:any;
+  patientId:any;
+  pId:any;
+  requestTime:any;
+  headers = ["ID","FAMILY NAME", "NAME", "GENDER", "RESOURCE TYPE", "BIRTH DATE"];
+  name:string;
+  dob:any;
+  loadingText:string;
 
   constructor(
     private apiService: ApiService
   ) { }
 
   ngOnInit() {
+    this.loadingText = "";
     this.apiService.getPatients().subscribe(
       data => {
         console.log(data);
+        this.requestTime = new Date().getTime();
+        this.patientData = data;
+        this.patientEntry = this.patientData.entry;
+        if(this.patientEntry == undefined){
+          this.loadingText = "No Result Found";
+        }
       }
     )
   }
+
+  getAllBetween(){ //data on DOB in 1965-1960 range
+    this.loadingText = "";
+    this.apiService.getAll().subscribe(
+      response => {
+        this.requestTime = new Date().getTime();
+        this.patientData = response;
+        this.patientEntry = this.patientData.entry;
+        if(this.patientEntry == undefined){
+          this.loadingText = "No Result Found";
+        }
+      }
+    )
+  }
+
+  patientList(n,d){ //data based on name and DOB input field
+    this.loadingText = "";
+    if(n!="" && d!=""){
+      this.name = n;
+      this.dob = d;
+      this.apiService.patientListQuery(this.name,this.dob).subscribe(
+        response => {
+          this.requestTime = new Date().getTime();
+          this.patientData = response;
+          this.patientEntry = this.patientData.entry;
+          if(this.patientEntry == undefined){
+            this.loadingText = "No Result Found";
+          }
+        }
+      )
+    }
+    
+  }
+
 }
 
 
--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -16,6 +16,16 @@ export class ApiService {
       { headers: this.getHeaders() });
   }
 
+  getAll() { //fetch based on DOB in 1965-1960 range
+    return this.httpClient.get(environment.queryURI + '/Patient?_sort=-birthdate&birthdate=gt1960-01-01&birthdate=lt1965-12-31',
+    { headers: this.getHeaders() });
+  }
+
+  patientListQuery(n,d){ //fetch based on name and DOB input field
+    return this.httpClient.get(environment.queryURI + '/Patient?_sort=-birthdate&birthdate='+d+'&name='+n,
+    { headers: this.getHeaders() });
+  }
+
   private getHeaders(): HttpHeaders {
     const headers = new HttpHeaders({
       'Content-Type': 'application/fhir+json'
